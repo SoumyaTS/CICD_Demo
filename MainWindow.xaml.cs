f930ce85513872f109c5106c036af979e153047a
SoumyaTS <t26soumya@gmail.com>
2023-05-08 00:27:19 +0530

    First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using SampleAppCICD.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+
+namespace SampleAppCICD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filePath = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+            filePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Details details = new Details();
+            details.name = nameTxt.Text;
+            details.phoneNum = phn.Text;
+            details.email = emaillbl.Text;
+            details.dob = dobtxt.Text;
+            details.address = addressTxt.Text;
+            detailserlization(details);
+        }
+        public void detailserlization(Details details)
+        {
+            var json = JsonSerializer.Serialize(details);
+            string combinefilePath = filePath + "\\RegistrationDetail.json";
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            System.IO.File.WriteAllText(combinefilePath, json);
+        }
+    }
+}
---
+++ Model/Details.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SampleAppCICD.Model
+{
+    public class Details
+    {
+        public string name { get; set; }
+        public string phoneNum { get; set; }
+        public string email { get; set; }
+        public string dob { get; set; }
+        public string address { get; set; }
+    }
+}
